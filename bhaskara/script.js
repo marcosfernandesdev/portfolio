56ed42868bebf7cc0eb6bdb95de755a8a84edc2f
marcosfernandesdev <95309210+marcosfernandesdev@users.noreply.github.com>
2022-01-03 11:23:11 -0300

    alterações 03/01

--- bhaskara/script.js
+++ bhaskara/script.js
@@ -1,23 +1,39 @@
-var a = document.getElementById('a')
+var a = document.getElementById('a');
 var b = document.getElementById('b');
 var c = document.getElementById('c');
 var formu = document.getElementById('formula')
 var res = document.getElementById('resultado');
 a.focus();
 
-function calcular(){
-    a = Number(a.value);
-    b = Number(b.value);
-    c = Number(c.value);
-    
-    let delta = 0;
-    delta = b**2 - (4*a*c);
-    let raiz_delta = Math.sqrt(delta);
-
-    let x1 = (-b + raiz_delta) / (2*a);
-    let x2 = (-b - raiz_delta) / (2*a);
-
-    formu.innerHTML = -b + '±' + '√' + raiz_delta + '<br> <hr>' + 2*a;
-    res.innerHTML += '<br><br>X1=  '+ x1 + '<br> X2=  ' + x2;
+function preenchido(){
+    let aS = String(a.value);
+    let bS = String(b.value);
+    let cS = String(c.value);
+    if (aS.length > 0 && bS.length > 0 && cS.length > 0 ){
+        return true
+    }
 }
 
+function calcular(){
+
+    if (preenchido()){
+
+        a = Number(a.value);
+        b = Number(b.value);
+        c = Number(c.value);
+        
+        let delta = 0;
+        delta = b**2 - (4*a*c);
+        let raiz_delta = Math.sqrt(delta);
+        
+        let x1 = (-b + raiz_delta) / (2*a);
+        let x2 = (-b - raiz_delta) / (2*a);
+        
+        formu.innerHTML = -b + '±' + '√' + raiz_delta + '<br> <hr>' + 2*a;
+        res.innerHTML = '<br><br>X1=  '+ x1 + '<br> X2=  ' + x2;
+    } else {
+        alert("Preeencha os campos.")
+    }
+    
+    
+}
--- imc/script.js
+++ imc/script.js
@@ -2,33 +2,43 @@ var peso = document.getElementById('peso')
 var altura = document.getElementById('altura')
 var res = document.getElementById('res')
 var sit = document.getElementById('sit')
-
+function preenchido(p, a){
+    let pS = String(p.value)
+    let aS = String(a.value) 
+    if (pS.length > 0 && aS.length > 0){
+        return true
+    }
+}
 function calcular(){
-    let p = Number(peso.value)
-    let a = Number(altura.value)
-    let imc = (p / (a / 100)**2)
-    res.innerHTML = `Seu IMC é: ${imc.toFixed(2)} \n`
-    if (imc <= 18.5){
-        //'magreza'
-        sit.innerText = 'Magreza'
-        sit.style.color = 'red'
-    } else if (imc <= 24.9){
-        //'normal'
-        sit.innerText = 'Normal'
-        sit.style.color = 'green'
-    } else if (imc <= 29.9) {
-        //'sobrepeso'
-        sit.innerText = 'Sobrepeso'
-        sit.style.color = 'yellow'
-    } else if(imc <= 39,9) {
-        //'obesidade'
-        sit.innerText = 'Obesidade'
-        sit.style.color = 'orange'
-    } else if (imc >= 40) {
-        //'obesidade grave'
-        sit.innerText = 'Obesidade grave'
-        sit.style.color = 'red'
+    if (preenchido(peso, altura)){
+        let p = Number(peso.value)
+        let a = Number(altura.value)
+        let imc = (p / (a / 100)**2)
+        res.innerHTML = `Seu IMC é: ${imc.toFixed(2)} \n`
+        if (imc <= 18.5){
+            //'magreza'
+            sit.innerText = 'Magreza'
+            sit.style.color = 'red'
+        } else if (imc <= 24.9){
+            //'normal'
+            sit.innerText = 'Normal'
+            sit.style.color = 'green'
+        } else if (imc <= 29.9) {
+            //'sobrepeso'
+            sit.innerText = 'Sobrepeso'
+            sit.style.color = 'yellow'
+        } else if(imc <= 39,9) {
+            //'obesidade'
+            sit.innerText = 'Obesidade'
+            sit.style.color = 'orange'
+        } else if (imc >= 40) {
+            //'obesidade grave'
+            sit.innerText = 'Obesidade grave'
+            sit.style.color = 'red'
+        } else {
+            alert('wtf?? kkkjjk')
+        }
     } else {
-        alert('wtf?? kkkjjk')
+        alert('Peencha os campos.')
     }
 }
